f5cfb2f7ad50136b1a4c62578c80eabb355d8bfc
matthewkilleen0830 <matthew.killeen0830@gmail.com>
2021-04-21 12:56:38 -0500

    Function created and when called, pulls in data.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,31 @@
 // Verify that app.js is loaded by index.html
-console.log("JavaScript app file is loaded.")
+console.log("JavaScript app file is loaded.");
+
+// START OF CODE GIVEN IN CLASS
+
+// Create function
+function initialDashboard() {
+
+    // Populate dropdown menu
+    var selector = d3.select("#selDataset");
+
+    // Use the D3 library to read in samples.json
+    d3.json("samples.json").then(function(data) {
+
+        // Verify data has been read in
+        console.log(data);
+
+    });
+
+    // Update bar chart plot with selected info from dropdown
+    // Update bubble chart plot with selected info from dropdown
+    // Update demographic info panel with selected info from dropdown
+
+}
+
+// Call function
+initialDashboard();
+
+// Verify function has been called
+console.log("initialDashboard() has been called.")
 
