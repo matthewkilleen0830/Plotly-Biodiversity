902a0ee9220c6191014561cb6b131c8281e29a4f
matthewkilleen0830 <matthew.killeen0830@gmail.com>
2021-04-23 12:18:07 -0500

    Added additional width parameter to make gauge chart responsive.

--- data/static/js/app.js
+++ data/static/js/app.js
@@ -188,7 +188,7 @@ function drawGaugeChart(wfreq) {
 
         // Define gauge chart layout
         var gaugeLayout = {
-            // width:  500,
+            width:  window.innerWidth / 1.4,
             // height:  500,
             margin:  {t:  0, r:  0, l:  0, b:  0},
         };
